14dac31a5f8b8eb126580dac6bc98c8e1e8ed0e0
Ritesh Patel <129499361+riteshpatel1884@users.noreply.github.com>
2026-03-17 22:40:56 +0530

    Update 1.java

--- TCS/2025/1.java
+++ TCS/2025/1.java
@@ -4,8 +4,6 @@ integer values. The task is to find the empty packets(0) of chocolate and push i
 Example 1 :
 N=8 and arr = [4,5,0,1,9,0,5,0].
 There are 3 empty packets in the given set. These 3 empty packets represented as O should be pushed towards the end of 
-the array
- 
 Input :
 8  – Value of N
 [4,5,0,1,9,0,5,0] – Element of arr[O] to arr[N-1],While input each element is separated by newline
