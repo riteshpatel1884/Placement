38c37e05955659ae8e54fd4954f1c56333a9c3d2
Ritesh Patel <129499361+riteshpatel1884@users.noreply.github.com>
2026-03-17 22:41:54 +0530

    Update 5.java

--- TCS/2025/5.java
+++ TCS/2025/5.java
@@ -1,8 +1,4 @@
-A supermarket maintains a pricing format for all its products. A value N is printed on each product. When the scanner reads 
-the value N on the item, the product of all the digits in the value N is the price of the item. The task here is to design 
-the software such that given the code of any item N the product (multiplication) of all the digits of value should be 
-computed(price).
-  
+A supermarket maintains a pricing
 Example 1:
 Input :
 5244 -> Value of N
